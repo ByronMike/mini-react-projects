4ef4603d12db7ea1da82270d2270225b907a6a32
ByronMike <auger.michaell@gmail.com>
2023-05-11 16:19:32 +0200

    commit

--- 01-pokedex/components/Home.js
+++ 01-pokedex/components/Home.js
@@ -1,23 +1,28 @@
 import styles from "../styles/Home.module.css";
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 import Card from "./Card";
 
 function Home() {
   const [dataPokedex, setDataPokedex] = useState([]);
-  useEffect( () => {
-      for(const i = 1; i < 152; i++){
-          fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-          .then(response => response.json())
-          .then(data => setDataPokedex(data))
-      }
-  },[])
-  // const allPokemon.map(dataPokedex, i) => {
-  // }
+
+  useEffect(() => {
+    for (let i = 1; i < 152; i++) {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+        .then((response) => response.json())
+        .then((data) => setDataPokedex((prevState) => [...prevState, data]));
+    }
+  }, []);
+
+  console.log(dataPokedex);
+
+  const allPokemon = dataPokedex.map((data, i) => (
+    <Card key={i} data={data} />
+  ));
 
   return (
     <>
       <div className={styles.title}>Pokedex</div>
-      <Card data={dataPokedex}/>
+      {allPokemon}
     </>
   );
 }
