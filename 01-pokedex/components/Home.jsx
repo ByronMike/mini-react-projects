bd85f299ca4a1b51d4e4e492f2134806aa8be543
Michael <auger.michaell@gmail.com>
2023-05-12 20:21:05 +0200

    fix display and elements position

--- 01-pokedex/components/Home.jsx
+++ 01-pokedex/components/Home.jsx
@@ -48,16 +48,41 @@ function Home() {
 
   return (
     <div className={styles.main}>
-      <div className={styles.header} style={{ position: "fixed", top: 0, left: 0, right: 0 }}>
+      <div
+        className={styles.header}
+        style={{
+          position: "relative",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          height: "50px",
+          top: 0,
+        }}
+      >
         <div className={styles.title}>Pokedex</div>
         <input className={styles.input} onChange={searchPokemon}></input>
       </div>
-      <div className={styles.cardsList} style={{ display: "flex", flexWrap: "wrap", alignItems: "flex-start" }}>
-        <div className={styles.cardsArray} style={ {display: "flex", justifyContent: "center", margin: "0 auto", maxWidth: "1200px", padding: "20px", gap: "20px", marginTop: "100px"} }>{allPokemon}</div>
+      <div
+        className={styles.cardsList}
+        style={{ display: "flex", flexWrap: "wrap", alignItems: "flex-start" }}
+      >
+        <div
+          className={styles.cardsArray}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            margin: "0 auto",
+            maxWidth: "1200px",
+            padding: "20px",
+            gap: "20px",
+            marginTop: "100px",
+          }}
+        >
+          {allPokemon}
+        </div>
       </div>
     </div>
   );
-  
 }
 
 export default Home;
